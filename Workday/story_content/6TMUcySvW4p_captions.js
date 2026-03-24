8b3ede46c2f05ed5c84695c1695b5835cf49796d
BeckySmith17 <andybeckysmith@gmail.com>
2026-03-24 15:20:44 -0400

    Initial project upload

---
+++ Workday/story_content/6TMUcySvW4p_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6TMUcySvW4p_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.112.36406.0%0D%0A%0D%0A00:00:00.300%20--%3E%2000:00:03.109%0D%0AA.%20%0D%0ATrue%0D%0AB.%20%0D%0AFalse%0D%0A%0D%0A"}]})
